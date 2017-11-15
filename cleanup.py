f668ae7f22aa6fb66b98ec18d3ae6e770c604274
Tom Brady <you@example.combradyte@gmail.com>
2017-11-15 13:21:37 -0500

    need to figure out how to normalize everything

---
+++ cleanup.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 12:19:16 2017
+
+@author: tbrady
+"""
+
+import sys, os
+sys.path.append("../..")
+import numpy as np
+from PIL import Image
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+
+def printVector(v, dims):
+    e = v.reshape(dims)
+    e = normalize(e,0,255)
+    plt.axis('off')
+    plt.imshow(e, cmap=cm.binary)
+    
+    
+    
+
+def normalize(X, low, high, dtype=None):
+    X = np.asarray(X)
+    minX , maxX = np.min(X), np.max(X)
+    # normalize to [0...1].
+    X = X - float(minX)
+    X = X / float((maxX - minX))
+    # scale to [low...high].
+    X = X * (high-low)
+    X = X + low
+    if dtype is None:
+        return np.asarray(X)
+    return np.asarray(X, dtype=dtype) 
+
+
+def subplot(title, images, rows, cols, sptitle="subplot", \
+            sptitles=[], colormap=cm.gray, ticks_visible=True,\
+            filename=None):
+    fig = plt.figure()
+    # main title
+    fig.text(.5, .95, title, horizontalalignment='center')
+    for i in range(len(images)):
+        ax0 = fig.add_subplot(rows,cols,(i+1))
+        plt.setp(ax0.get_xticklabels(), visible=False)
+        plt.setp(ax0.get_yticklabels(), visible=False)
+#        if len(sptitles) == len(images):
+#            plt.title("%s #%s" % (sptitle, str(sptitles[i])))
+#        else:
+#            plt.title("%s #%d" % (sptitle, (i+1)))
+        plt.imshow(np.asarray(images[i]), cmap=colormap)
+        plt.axis('off')
+    if filename is None:
+        plt.show()
+    else:
+        fig.savefig(filename)
+
+# read in all the images
+def readImages(path, sz=None): 
+    c = 0
+    X,y = [], []
+    for dirname , dirnames , filenames in os.walk(path):
+        for subdirname in dirnames:
+            subject_path = os.path.join(dirname , subdirname)
+            for filename in os.listdir(subject_path):
+                try:
+                    im = Image.open(os.path.join(subject_path , filename))
+                    im = im.convert("L")
+                    # resize to given size (if given)
+                    if (sz is not None):
+                        im = im.resize(sz, Image.ANTIALIAS)
+                    X.append(np.asarray(im, dtype=np.uint8))
+                    y.append(c)
+                except IOError as err:
+                    print ("I/O error: {0}".format(err))
+                except:
+                    print ("Unexpected error:", sys.exc_info()[0])
+                    raise
+                c = c+1
+    return [X,y]
--- ece5831_hw2.py
+++ ece5831_hw2.py
@@ -11,140 +11,78 @@ import numpy as np
 from PIL import Image
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
+from cleanup import *
 
 
-def normalize(X, low, high, dtype=None):
-    X = np.asarray(X)
-    minX , maxX = np.min(X), np.max(X)
-    # normalize to [0...1].
-    X = X - float(minX)
-    X = X / float((maxX - minX))
-    # scale to [low...high].
-    X = X * (high-low)
-    X = X + low
-    if dtype is None:
-        return np.asarray(X)
-    return np.asarray(X, dtype=dtype)    
+if 1:
+    # read in the faces and number of images
+    [trainingFaces,trainingFacesCount] = readImages('/Users/tbrady/drive/sw/att_faces/')
+    
+    # dimensions of the picture size
+    dims = trainingFaces[0].shape
+    
+    # initialize an empty vector of m x n length
+    L = np.empty((trainingFaces[0].size, 0), dtype='float64')
+    
+    # create a matrix, gamma, of image vectors as long as the number of faces
+    for col in trainingFaces:
+        L = np.hstack((L ,np.asarray(col).reshape(-1, 1)))
+    
+#    for col in L:
+#        L[:,int(col)] = (L[:,int(col)]/ np.linalg.norm(L[:,int(col)], axis=1))
+    # average the vectors to create and average face
+    meanVector  = np.array(L.mean(axis=1))
+    
+    # subtract the mean vector from the original images
+    #t he double transpose is due to how the vectors are handled in numpy
+    A       = (L.T - meanVector).T
+    
+    # the dot product of transposed difference matrix and original matrix
+    C       = np.dot(A.T,A)
+    
+    # calculate the eigenvectors and eigenvalues
+    [v, u]  = np.linalg.eig(C)
+    
+    # sort them by largest eigenvalues
+    idx     = np.argsort(-v)
+    
+    # reorganize the eigenvalues by the sorted index
+    v       = v[idx]
+    
+    # reorganize the eigenvectors by the sorted index
+    u       = u[:,idx]
+    
+    # create the eigen faces
+    U       = np.dot(A, u)  
 
-def subplot(title, images, rows, cols, sptitle="subplot", \
-            sptitles=[], colormap=cm. gray, ticks_visible=True,\
-            filename=None):
-    fig = plt.figure()
-    # main title
-    fig.text(.5, .95, title, horizontalalignment='center')
-    for i in range(len(images)):
-        ax0 = fig.add_subplot(rows,cols,(i+1))
-        plt.setp(ax0.get_xticklabels(), visible=False)
-        plt.setp(ax0.get_yticklabels(), visible=False)
-        if len(sptitles) == len(images):
-            plt.title("%s #%s" % (sptitle, str(sptitles[i])))
-        else:
-            plt.title("%s #%d" % (sptitle, (i+1)))
-        plt.imshow(np.asarray(images[i]), cmap=colormap)
-    if filename is None:
-        plt.show()
-    else:
-        fig.savefig(filename)
-
-# read in all the images
-def readImages(path, sz=None): 
-    c = 0
-    X,y = [], []
-    for dirname , dirnames , filenames in os.walk(path):
-        for subdirname in dirnames:
-            subject_path = os.path.join(dirname , subdirname)
-            for filename in os.listdir(subject_path):
-                try:
-                    im = Image.open(os.path.join(subject_path , filename))
-                    im = im.convert("L")
-                    # resize to given size (if given)
-                    if (sz is not None):
-                        im = im.resize(sz, Image.ANTIALIAS)
-                    X.append(np.asarray(im, dtype=np.uint8))
-                    y.append(c)
-                except IOError as err:
-                    print ("I/O error: {0}".format(err))
-                except:
-                    print ("Unexpected error:", sys.exc_info()[0])
-                    raise
-                c = c+1
-    return [X,y]
+    # normalize the eigenface matrix U   
+    U       = (U.T / np.linalg.norm(U, axis=1)).T
 
 
-[trainingFaces,trainingFacesNum] = readImages('/Users/tbrady/drive/sw/att_faces/')
-
-# create a matrix of y columns where each image is stored in one row
-L = np.empty((trainingFaces[0].size, 0), dtype='float64')
-for col in trainingFaces:
-    L = np.hstack((L ,np.asarray(col).reshape(-1, 1)))
-
-averageFaceVector   = np.array(L.mean(axis=1))
-
-phiMatrix           = (L.transpose() - averageFaceVector).transpose()
-
-eigenMatrix         = np.dot(phiMatrix.T,phiMatrix)
-[eigenvalues ,eigenvectors] = np.linalg.eig(eigenMatrix)
-idx                 = np.argsort(-eigenvalues)
-eigenvalues         = eigenvalues[idx]
-eigenvectors        = eigenvectors[:,idx]
-
-
-numComponents       = 50
-eigenvalues         = eigenvalues[0:numComponents].copy()
-eigenvectors        = eigenvectors[:, 0:numComponents].copy()
-
-eigenvectors        = np.dot(L, eigenvectors)
-#norms               = np.linalg.norm(eigenvectors, axis=0)
-#eigenvectors        /= norms
-
-
-
-def printVector(v):
-    e = v.reshape(trainingFaces[0].shape)
-    e = normalize(e,0,255)
-    plt.imshow(e)
-
+numFaces = 8
 E = []
-for i in range(min(len(L), 4)):
-    e = eigenvectors[:,i].reshape(trainingFaces[0].shape)
+for i in range(0, numFaces):
+    e = U[:,i].reshape(dims)
     E.append(normalize(e,0,255))
-#    # plot them and store the plot to "python_eigenfaces.pdf"
 
-subplot(title="Eigenfaces", images=E, rows=1, \
-        cols=4, sptitle=" Eigenface", colormap=cm.binary, \
+#printVector(meanVector, dims)
+
+subplot(title="Eigenfaces", images=E, rows=numFaces/4, \
+        cols=4, sptitle=" Eigenface", colormap=cm.gist_yarg, \
         filename="python_pca_eigenfaces.png")
 
-##steps = [i for i in range(10, min(len(trainingFaceMatrix),320), 20)]
-steps = [10,20]
-E = []
-for i in range(min(len(steps),16)):
-    numEvs = steps[i]
-#    P = project(eigenvectors[:,0:numEvs],trainingFaces[0].reshape(-1, 1) , averageFaceVector)
-    dotFace = np.dot(L[:,0:numEVs] - averageFaceVector, eigenvectors[:,0:numEVs])
-#    R = reconstruct(eigenvectors[:,0:numEvs ], P , averageFaceVector)
-#    
-#    
-#    R = R.reshape(trainingFaces[0].shape)
-#    E.append(normalize(R,0,255))
 
-
-
-
-
-
-w = np.dot(eigenvectors[:,0], trainingFaceMatrix[:,0] - averageFaceVector)
-printVector(averageFaceVector + np.dot(dotFace,eigenvectors[:,0].T))
-
+#faces = 30
+#reconFace = meanVector
+##
+#psi = L[:,0] - meanVector
+#w = np.dot(U[:,0].T, psi)
+#w = np.dot(U[:,0:faces].T, psi)
+#for i in range(0,faces):
+#    reconFace += w[i]*U[:,i]
 #
-#def project (W, X ,mu = None):
-#    if mu is None :
-#        return np.dot(X, W)
-#    return np.dot(X - mu, W)
-#
-#def reconstruct (W, Y, mu = None ):
-#    if mu is None:
-#        return np.dot(Y, W.T)
-#    return np.dot(Y, W.T) + mu
+#printVector(reconFace, dims)
+
 #
 ##steps = [i for i in range(10, min(len(trainingFaceMatrix),320), 20)]
 #steps = [10,20]
@@ -157,30 +95,4 @@ printVector(averageFaceVector + np.dot(dotFace,eigenvectors[:,0].T))
 #    
 #    R = R.reshape(trainingFaces[0].shape)
 #    E.append(normalize(R,0,255))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
